c5cb7a7b43e078a2390ce7b5a4908c51211350f7
leozf13 <leonardo.zfacini@gmail.com>
2022-03-15 20:54:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//Dicas VS Code
+//ctrl + space aparece caixa de sugestões
+// shift + alt + s = formata o texto
+
+console.log("asfdsfasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssaafs")
+// ALT+Z = quebra de linha
+
+//Apertar e segurar o alt e mover a seta para cima ou baixo, move a linha de lugar
+
+//código apagado quer dizer que ele não está sendo usado ou executado (ctrl + .) aciona o quick fix
+
+//F8 leva para o próximo erro detectado no código
+
+//CTRL + F2 Seleciona todas as palavras chave repetidas e alterará todas e ctrl + D seleciona uma por vez
+
+//F2 abre uma caixa de texto e altera o nome em todos os campos aonde tem aquela variável
+
+const oiaaaaaaa = 10
+oiaaaaaaa = 25
+
+//f12 mostra aonde a variavel é declara pela primeira vez
+// ctrl + shift + k apaga a linha
